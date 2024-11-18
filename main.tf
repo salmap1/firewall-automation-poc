822c120d60fbde8a7611e258d7019535f6b6924e
Salma P [C] salmap <salmap@U-2ZIQPIMQ7KREH.bw-contractors.local>
2024-11-18 16:37:35 +0530

    Testing

--- main.tf
+++ main.tf
@@ -13,17 +13,18 @@ provider "panos" {
   password = var.password
 }
 
-# Call the NOC and IP team configurations
-module "noc_team" {
-  source    = "./noc"
-  hostname  = var.hostname
-  username  = var.username
-  password  = var.password
-}
-
 module "ip_team" {
   source    = "./ip"
   hostname  = var.hostname
   username  = var.username
   password  = var.password
 }
+
+module "noc_team" {
+  source    = "./noc"
+  hostname  = var.hostname
+  username  = var.username
+  password  = var.password
+}
+ 
+
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+hostname = "10.255.49.67"
+username = "salmap"
+password = "Plum@7890"
+
